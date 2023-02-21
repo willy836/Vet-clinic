c9000cd63358c579072cda1bd98b96df8fba9213
willy836 <wilsonkimiri836@gmail.com>
2023-02-21 20:07:51 +0300

    Query and update animals table

--- queries.sql
+++ queries.sql
@@ -23,3 +23,40 @@ WHERE name NOT LIKE 'Gabumon';
 
 SELECT * FROM animals
 WHERE weight_kg >= 10.40 AND weight_kg <= 17.30;
+
+/* Begin of a transaction */
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
+/* End */
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, MAX(escape_attempts) FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight 
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) as avg_escape_att
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
