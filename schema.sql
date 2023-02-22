86b664f37f8cecc2556c3e46ea0942570633f3c3
willy836 <wilsonkimiri836@gmail.com>
2023-02-22 22:39:40 +0300

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,41 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+/* Modify animals table */
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY(species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY(owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
+
+
+
