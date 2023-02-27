106598de5a9be5381fc87cd2c65acc2f0c4ba5f7
willy836 <wilsonkimiri836@gmail.com>
2023-02-27 15:33:06 +0300

    Database performance audit

--- data.sql
+++ data.sql
@@ -124,3 +124,13 @@ VALUES(1, 1, date '2020-05-24'),
 	(2, 9, date '2020-08-03'),
 	(3, 10, date '2020-05-24'),
 	(1, 10, date '2021-01-11');
+
+-- Database performance audit
+INSERT INTO visits (animals_id, vets_id, date_of_visit) 
+SELECT * FROM (SELECT id FROM animals) animal_ids, 
+(SELECT id FROM vets) vets_ids, 
+generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+insert into owners (full_name, email) 
+select 'Owner ' || generate_series(1,2500000), 'owner_' 
+|| generate_series(1,2500000) || '@mail.com';
